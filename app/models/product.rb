7ef4d49e3b0d43b0ebad22f39e120356b2f55172
Phat Chiem <thachphat0@gmail.com>
2016-10-26 20:12:39 +0700

    add final_price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,31 @@
 class Product < ActiveRecord::Base
-  before_save :set_default_image_path
-  validates :name, presence: true
+	before_save :set_default_image_path
+	validates :name, presence: true
 
-  private
+	def discount_amount
+		if self.price_vnd > 800000
+			return 41
+		elsif self.price_vnd > 200000
+			return 31
+		elsif self.price_vnd > 100000
+			return 21
+		else
+			return 0
+		end
+	end
 
-  def set_default_image_path
-    self.image_path ||= "http://lorempixel.com/200/200/fashion"
-  end
+	def final_price
+		return (self.price_vnd / 10000).round * 10000
+	end
 
-  def self.alphabetical
-  	return Product.all.order(:name)
-  end
+	private
+
+	def set_default_image_path
+		self.image_path ||= "http://lorempixel.com/200/200/fashion"
+	end
+
+	def self.alphabetical
+		return Product.all.order(:name)
+	end
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -21,3 +21,88 @@ describe '.alphabetical' do
 	  expect(Product.alphabetical).to eq([a,b,c])
 	end
 end
+
+describe 'On sale test' do
+	it '<= 100,000 VND' do
+		product = FactoryGirl.create(:product, price_vnd: 0)
+		expect(product.discount_amount).to eq 0
+
+		product = FactoryGirl.create(:product, price_vnd: 1)
+		expect(product.discount_amount).to eq 0
+
+		product = FactoryGirl.create(:product, price_vnd: 50000)
+		expect(product.discount_amount).to eq 0
+
+		product = FactoryGirl.create(:product, price_vnd: 99999)
+		expect(product.discount_amount).to eq 0
+
+		product = FactoryGirl.create(:product, price_vnd: 100000)
+		expect(product.discount_amount).to eq 0
+	end	
+
+	it '> 100,000 VND and <= 200,000 VND' do
+		product = FactoryGirl.create(:product, price_vnd: 100001)
+		expect(product.discount_amount).to eq 21
+
+		product = FactoryGirl.create(:product, price_vnd: 150000)
+		expect(product.discount_amount).to eq 21
+
+		product = FactoryGirl.create(:product, price_vnd: 199999)
+		expect(product.discount_amount).to eq 21
+
+		product = FactoryGirl.create(:product, price_vnd: 200000)
+		expect(product.discount_amount).to eq 21
+	end	
+
+	it '> 200,000 VND and <= 800,000 VND' do
+		product = FactoryGirl.create(:product, price_vnd: 200001)
+		expect(product.discount_amount).to eq 31
+
+		product = FactoryGirl.create(:product, price_vnd: 250000)
+		expect(product.discount_amount).to eq 31
+
+		product = FactoryGirl.create(:product, price_vnd: 799999)
+		expect(product.discount_amount).to eq 31
+
+		product = FactoryGirl.create(:product, price_vnd: 800000)
+		expect(product.discount_amount).to eq 31
+	end	
+
+	it '> 800,000 VND' do
+		product = FactoryGirl.create(:product, price_vnd: 800001)
+		expect(product.discount_amount).to eq 41
+
+		product = FactoryGirl.create(:product, price_vnd: 900000)
+		expect(product.discount_amount).to eq 41
+	end
+end
+
+describe 'Final price' do
+	it 'round up' do
+		product = FactoryGirl.create(:product, price_vnd: 115000)
+		expect(product.final_price).to eq 120000
+
+		product = FactoryGirl.create(:product, price_vnd: 115001)
+		expect(product.final_price).to eq 120000
+
+		product = FactoryGirl.create(:product, price_vnd: 119999)
+		expect(product.final_price).to eq 120000
+
+		product = FactoryGirl.create(:product, price_vnd: 120000)
+		expect(product.final_price).to eq 120000
+	end
+
+	it 'round down' do
+		product = FactoryGirl.create(:product, price_vnd: 114999)
+		expect(product.final_price).to eq 110000
+
+		product = FactoryGirl.create(:product, price_vnd: 113000)
+		expect(product.final_price).to eq 110000
+
+		product = FactoryGirl.create(:product, price_vnd: 110001)
+		expect(product.final_price).to eq 110000
+
+		product = FactoryGirl.create(:product, price_vnd: 110000)
+		expect(product.final_price).to eq 110000
+	end
+end	
