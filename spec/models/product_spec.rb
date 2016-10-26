c79f17b62c6086f4644a09a2a79e19812c792c46
Phat Chiem <thachphat0@gmail.com>
2016-10-26 19:43:44 +0700

    create product test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,3 +3,21 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe '.alphabetical' do
+	it "returns [] when there are no products" do
+	  expect(Product.alphabetical).to eq([])
+	end
+
+	it "returns [a] when there is only one product a" do
+	  a = Product.create!(name: 'a', description: 'description', image_path: '', price_vnd: 50, weight: 1)
+	  expect(Product.alphabetical).to eq([a])
+	end
+
+	it "returns [a,b,c] after I create 3 products c, b, a" do
+	  c = Product.create!(name: 'c', description: 'description', image_path: '', price_vnd: 50, weight: 1)
+	  b = Product.create!(name: 'b', description: 'description', image_path: '', price_vnd: 50, weight: 1)
+	  a = Product.create!(name: 'a', description: 'description', image_path: '', price_vnd: 50, weight: 1)
+	  expect(Product.alphabetical).to eq([a,b,c])
+	end
+end
